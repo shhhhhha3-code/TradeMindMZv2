9eb6bedfadd2d0a32434126b4871882399396b2d
shhhhhha3-code <shhhhhha3@gmail.com>
2026-09-19 23:38:42 +0200

    Set Android app name to TradeMindMZ

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.trademindmz.app",
-  appName: "TMZ21",
+  appName: "TradeMindMZ",
   webDir: "dist",
   server: {
     androidScheme: "https",
