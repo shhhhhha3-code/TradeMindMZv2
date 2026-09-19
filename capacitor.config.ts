0af5e738067434eb84db2efc8fb260a155ad84fc
shhhhhha3-code <shhhhhha3@gmail.com>
2026-09-19 08:57:53 +0200

    feat: rename Android app to TMZ21

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.trademindmz.app",
-  appName: "TradeMindMZ",
+  appName: "TMZ21",
   webDir: "dist",
   server: {
     androidScheme: "https",
